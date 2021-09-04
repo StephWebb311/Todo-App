49f251da9b804f7eea86511e030c24e9457ea8a4
StephWebb311 <61339968+StephWebb311@users.noreply.github.com>
2021-09-04 16:31:51 -0500

    fb

--- kanban-fire/src/environments/environment.prod.ts
+++ kanban-fire/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA5-fZRH_C07nrvBW1xfN0J1--38SJb4Uw",
+    authDomain: "kanbanfire-e7ad6.firebaseapp.com",
+    projectId: "kanbanfire-e7ad6",
+    storageBucket: "kanbanfire-e7ad6.appspot.com",
+    messagingSenderId: "580126970502",
+    appId: "1:580126970502:web:0acebc9219a1e1caf3b707",
+    measurementId: "G-JP6VW4PQPJ"
+  }
 };
--- kanban-fire/src/environments/environment.ts
+++ kanban-fire/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA5-fZRH_C07nrvBW1xfN0J1--38SJb4Uw",
+    authDomain: "kanbanfire-e7ad6.firebaseapp.com",
+    projectId: "kanbanfire-e7ad6",
+    storageBucket: "kanbanfire-e7ad6.appspot.com",
+    messagingSenderId: "580126970502",
+    appId: "1:580126970502:web:0acebc9219a1e1caf3b707",
+    measurementId: "G-JP6VW4PQPJ"
+  }
 };
 
 /*
